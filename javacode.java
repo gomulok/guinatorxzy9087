1ad0cc2343273acffd3c6cd9fb3f6a257b5b034f
gomulok <121164050+gomulok@users.noreply.github.com>
2023-06-05 10:30:34 +0300

    Add files via upload

---
+++ javacode.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class javacode {
+    public static void main(String[] args) {
+        Scanner scannest = new Scanner(System.in);
+        scannest.nextLine();
+    }
+}
