0a0a1daeb0e890248c0a12fe694ef9c7af0e4da4
gomulok <121164050+gomulok@users.noreply.github.com>
2023-06-08 17:16:15 +0300

    Add files via upload

--- javacode.java
+++ javacode.java
@@ -1,8 +1,22 @@
+import java.util.Objects;
 import java.util.Scanner;
 
-public class javacode {
+public class javacode{
     public static void main(String[] args) {
-        Scanner scannest = new Scanner(System.in);
-        scannest.nextLine();
+        System.out.print("Are you happy?(please, write true, false or unknow: ");
+        Scanner scan = new Scanner(System.in);
+        String answer = scan.nextLine();
+        if(Objects.equals(answer, "true")){
+            System.out.println("Ok");
+        }
+        else if(Objects.equals(answer, "false")){
+            System.out.println("Why you aren;t happy");
+        }
+        else if(Objects.equals(answer, "unknow")){
+            System.out.println("How you unknow you happy?");
+        }
+        else{
+            System.out.println("please write true, false or unknow");
+        }
     }
 }
