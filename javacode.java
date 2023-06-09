7a1733dbc04caf9790c60e3c13dec336df2e74d2
gomulok <121164050+gomulok@users.noreply.github.com>
2023-06-09 10:07:45 +0300

    Add files via upload

--- javacode.java
+++ javacode.java
@@ -1,22 +1,25 @@
 import java.util.Objects;
 import java.util.Scanner;
 
-public class javacode{
-    public static void main(String[] args) {
-        System.out.print("Are you happy?(please, write true, false or unknow: ");
-        Scanner scan = new Scanner(System.in);
-        String answer = scan.nextLine();
-        if(Objects.equals(answer, "true")){
-            System.out.println("Ok");
-        }
-        else if(Objects.equals(answer, "false")){
-            System.out.println("Why you aren;t happy");
-        }
-        else if(Objects.equals(answer, "unknow")){
-            System.out.println("How you unknow you happy?");
-        }
-        else{
-            System.out.println("please write true, false or unknow");
-        }
-    }
+public class javacode {
+	public static void main(String[] args) {
+		boolean code = true;
+		while (code) {
+			System.out.print("Are you happy?(please, write true, false or unknow: ");
+			Scanner scan = new Scanner(System.in);
+			String answer = scan.nextLine();
+			if (Objects.equals(answer, "true")) {
+				System.out.println("Ok");
+				code = false;
+			} else if (Objects.equals(answer, "false")) {
+				System.out.println("Why you aren;t happy");
+			code = false;
+			} else if (Objects.equals(answer, "unknow")) {
+				System.out.println("How you unknow you happy?");
+			code = false;
+			} else {
+				System.out.println("please write true, false or unknow");
+			}
+		}
+	}
 }
